6cd6091a7f6056c2633946e73d1ef6b41ffc81c2
Juan Palma <juanpedropalma@hotmail.com>
2021-01-07 02:57:08 +0000

    First complete draft

--- assets/javascripts/my_website.js
+++ assets/javascripts/my_website.js
@@ -4,9 +4,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const cursor = document.querySelector('.cursor');
   setInterval(() => {
     cursor.classList.toggle('hidden');
-  }, 100);
-<<<<<<< HEAD
+  }, 800);
 });
-=======
-});
->>>>>>> 5e222829aca59fe5546d3571707d219f603d6597
